359d6f039641190bfa29e550e599aaf1bd51b4e7
sheikhabbasit <sheikh.abdul.basit95@gmail.com>
2022-02-16 15:36:53 +0530

    refactor(Dashboard): Add a new component for Tabs

--- Screens/Dashboard.js
+++ Screens/Dashboard.js
@@ -1,47 +1,33 @@
 import {View, Text, ScrollView, StyleSheet, Pressable} from 'react-native';
 import React, {useState} from 'react';
+import Tabs from '../components/Tabs';
 
 const Dashboard = props => {
   const [activeTab, setActiveTab] = useState(0);
   const {name, email, address, mobileNo, age} = props.route.params.data;
+  const publicData = [name, email, age];
+  const privateData = [address, mobileNo];
   return (
     <View style={styles.wrapper}>
       <ScrollView>
-        <View style={styles.navigationWrapper}>
-          <Pressable
-            android_ripple={{color: 'white'}}
-            style={[styles.navItem, activeTab === 0 && styles.activeNav]}
-            onPress={() => setActiveTab(0)}>
-            <Text style={styles.navItemLabel}>Public Data</Text>
-          </Pressable>
-          <Pressable
-            android_ripple={{color: 'white'}}
-            style={[styles.navItem, activeTab === 1 && styles.activeNav]}
-            onPress={() => setActiveTab(1)}>
-            <Text style={styles.navItemLabel}>Private Data</Text>
-          </Pressable>
-        </View>
+        <Tabs activeTab={activeTab} setActiveTab={setActiveTab} />
+
         {activeTab === 0 && (
           <View>
-            <View style={styles.contentWrapper}>
-              <Text style={styles.label}>{name}</Text>
-            </View>
-            <View style={styles.contentWrapper}>
-              <Text style={styles.label}>{email}</Text>
-            </View>
-            <View style={styles.contentWrapper}>
-              <Text style={styles.label}>{age}</Text>
-            </View>
+            {publicData.map((data, index) => (
+              <View style={styles.contentWrapper} key={index}>
+                <Text style={styles.label}>{data}</Text>
+              </View>
+            ))}
           </View>
         )}
         {activeTab === 1 && (
           <View>
-            <View style={styles.contentWrapper}>
-              <Text style={styles.label}>{address}</Text>
-            </View>
-            <View style={styles.contentWrapper}>
-              <Text style={styles.label}>{mobileNo}</Text>
-            </View>
+            {privateData.map((data, index) => (
+              <View style={styles.contentWrapper} key={index}>
+                <Text style={styles.label}>{data}</Text>
+              </View>
+            ))}
           </View>
         )}
       </ScrollView>
@@ -54,26 +40,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#393E46',
     flex: 1,
     padding: 20,
-  },
-  navigationWrapper: {
-    backgroundColor: '#3FC1C9',
-    borderRadius: 5,
-    flexDirection: 'row',
-    justifyContent: 'space-evenly',
-  },
-  navItem: {
-    padding: 10,
-    borderColor: 'grey',
-    borderWidth: 1,
-    width: '50%',
-    alignItems: 'center',
-  },
-  activeNav: {
-    backgroundColor: '#3F90a7',
-  },
-  navItemLabel: {
-    color: '#fff',
-    fontSize: 16,
   },
   contentWrapper: {
     marginTop: 30,
---
+++ components/Tabs.js
@@ -0,0 +1,46 @@
+import {View, Text, Pressable, StyleSheet} from 'react-native';
+import React from 'react';
+
+const Tabs = ({activeTab, setActiveTab}) => {
+  const tabs = [
+    {name: 'Public Data', index: 0},
+    {name: 'Private Data', index: 1},
+  ];
+  return (
+    <View style={styles.navigationWrapper}>
+      {tabs.map((tab, index) => (
+        <Pressable
+          android_ripple={{color: 'white'}}
+          style={[styles.navItem, activeTab === tab.index && styles.activeNav]}
+          onPress={() => setActiveTab(index)}>
+          <Text style={styles.navItemLabel}>{tab.name}</Text>
+        </Pressable>
+      ))}
+    </View>
+  );
+};
+
+export default Tabs;
+
+const styles = StyleSheet.create({
+  navigationWrapper: {
+    backgroundColor: '#3FC1C9',
+    borderRadius: 5,
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+  },
+  navItem: {
+    padding: 10,
+    borderColor: 'grey',
+    borderWidth: 1,
+    width: '50%',
+    alignItems: 'center',
+  },
+  activeNav: {
+    backgroundColor: '#3F90a7',
+  },
+  navItemLabel: {
+    color: '#fff',
+    fontSize: 16,
+  },
+});
