2953a4f271b3c2b625536235aced9ccc0b620f5e
sheikhabbasit <sheikh.abdul.basit95@gmail.com>
2022-02-16 11:29:26 +0530

    feat(App, StackNav, TabNav): Add stack and tab navigation

--- App.js
+++ App.js
@@ -1,19 +1,11 @@
 import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import Home from './Home';
-import Main from './Main';
-import Profile from './Profile';
+import BottomTabNavigator from './TabNavigator';
 
-const Stack = createNativeStackNavigator();
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Main">
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Main" component={Main} />
-        <Stack.Screen name="Profile" component={Profile} />
-      </Stack.Navigator>
+      <BottomTabNavigator />
     </NavigationContainer>
   );
 };
