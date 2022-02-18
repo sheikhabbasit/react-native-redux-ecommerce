29534ec8e0069312b6f76ec600a3a233c73a27ed
sheikhabbasit <sheikh.abdul.basit95@gmail.com>
2022-02-18 17:19:14 +0530

    feat(dashboard, profie): added dashboard and profile

--- Screens/Dashboard.js
+++ Screens/Dashboard.js
@@ -1,44 +1,15 @@
-import {View, Text, ScrollView, StyleSheet, Pressable} from 'react-native';
+import {View, TextInput, ScrollView, StyleSheet, Pressable} from 'react-native';
 import React, {useState} from 'react';
-import Tabs from '../components/Tabs';
+import Tabs from '../Source/Components/Tabs';
 
 const Dashboard = props => {
   const [activeTab, setActiveTab] = useState(0);
-  const {name, email, address, mobileNo, age} = props.route.params.data;
-  const publicData = [
-    {fieldValue: name, label: 'Name'},
-    {fieldValue: email, label: 'Email'},
-    {fieldValue: age, label: 'Age'},
-  ];
-  const privateData = [
-    {fieldValue: address, label: 'Address'},
-    {fieldValue: mobileNo, label: 'Mobile No.'},
-  ];
+
   return (
     <View style={styles.wrapper}>
       <ScrollView>
         <Tabs activeTab={activeTab} setActiveTab={setActiveTab} />
-
-        {activeTab === 0 && (
-          <View>
-            {publicData.map((data, index) => (
-              <View style={styles.contentWrapper} key={data.label}>
-                <Text style={styles.contentLabel}>{data.label}:</Text>
-                <Text style={styles.label}>{data.fieldValue}</Text>
-              </View>
-            ))}
-          </View>
-        )}
-        {activeTab === 1 && (
-          <View>
-            {privateData.map((data, index) => (
-              <View style={styles.contentWrapper} key={data.label}>
-                <Text style={styles.contentLabel}>{data.label}:</Text>
-                <Text style={styles.label}>{data.fieldValue}</Text>
-              </View>
-            ))}
-          </View>
-        )}
+        <TextInput style={styles.input} />
       </ScrollView>
     </View>
   );
@@ -50,20 +21,8 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 20,
   },
-  contentWrapper: {
-    marginTop: 30,
-    padding: 10,
-    backgroundColor: '#fffccc',
-    flex: 1,
-    borderRadius: 5,
-  },
-  contentLabel: {
-    color: '#393E46',
-    fontWeight: 'bold',
-  },
-  label: {
-    fontSize: 16,
-    color: '#000',
+  input: {
+    backgroundColor: '#fff',
   },
 });
 
--- Screens/Profile.js
+++ Screens/Profile.js
@@ -9,19 +9,10 @@ import {
 import React, {useState} from 'react';
 
 const Profile = () => {
-  const [show, setShow] = useState(false);
   return (
     <SafeAreaView style={styles.parent}>
       <ScrollView>
-        <View style={styles.box}>
-          <Text>This is Profile Page</Text>
-          {show && <Text>Pressible at Work</Text>}
-        </View>
-        <Pressable style={styles.box} onPress={() => setShow(!show)}>
-          <Text>{show ? 'Hide' : 'Show'}</Text>
-          <Text>Pressible</Text>
-        </Pressable>
-        <View style={styles.box2} />
+        <Text>This is Profile Page</Text>
       </ScrollView>
     </SafeAreaView>
   );
@@ -30,25 +21,7 @@ const Profile = () => {
 export default Profile;
 const styles = StyleSheet.create({
   parent: {
-    // backgroundColor: 'blue',
     flex: 1,
-  },
-  wrapper: {
     backgroundColor: 'green',
-    flex: 1,
-    paddingHorizontal: 20,
-    marginHorizontal: 20,
-  },
-  box: {
-    backgroundColor: 'green',
-    height: 100,
-    marginBottom: 20,
-  },
-  box2: {
-    backgroundColor: 'brown',
-    height: 300,
-    marginBottom: 20,
-    marginEnd: 20,
-    width: 200,
   },
 });
