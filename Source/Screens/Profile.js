ffe6c775d76c7bdf8c94b92d46e5025a845523b0
sheikhabbasit <sheikh.abdul.basit95@gmail.com>
2022-02-23 15:33:06 +0530

    refactor(Profile.js): add redux, remove async storage

--- Source/Screens/Profile.js
+++ Source/Screens/Profile.js
@@ -1,26 +1,13 @@
-import {
-  View,
-  Text,
-  SafeAreaView,
-  ScrollView,
-  StyleSheet,
-  RefreshControl,
-  ActivityIndicator,
-} from 'react-native';
-import React, {useState, useEffect, useCallback} from 'react';
+import {Text, SafeAreaView, ScrollView, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
 import {useSelector} from 'react-redux';
 
 const Profile = props => {
-  const [refreshing, setRefreshing] = useState(false);
   const {email, password} = useSelector(state => state.app.userInfo);
-
-  useEffect(() => {}, [email, password]);
 
   return (
     <SafeAreaView style={styles.parent}>
-      <ScrollView
-        refreshControl={<RefreshControl refreshing={refreshing} />}
-        contentContainerStyle={styles.card}>
+      <ScrollView contentContainerStyle={styles.card}>
         <Text style={styles.textLabel}>
           Email ID: <Text style={styles.credentials}>{email}</Text>
         </Text>
