0d5e33275b71e5a2edd21bf3f17cbeb8a8dcaf37
sheikhabbasit <sheikh.abdul.basit95@gmail.com>
2022-02-23 13:08:13 +0530

    feat(App.js): provide redux store

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import PublicStackNavigator from './Source/Navigation/PublicStackNavigator';
+import {Provider} from 'react-redux';
+import {store} from './Source/Redux/Store';
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <PublicStackNavigator />
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <PublicStackNavigator />
+      </NavigationContainer>
+    </Provider>
   );
 };
 
