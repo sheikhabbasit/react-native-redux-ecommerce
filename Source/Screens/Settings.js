1c1dc32cd75bc7bc357705daec6e05df06bc27da
sheikhabbasit <sheikh.abdul.basit95@gmail.com>
2022-02-23 15:33:44 +0530

    refactor(Settings.js): add redux, remove async storage

--- Source/Screens/Settings.js
+++ Source/Screens/Settings.js
@@ -2,10 +2,13 @@ import {StyleSheet, Text, Pressable, SafeAreaView} from 'react-native';
 import React from 'react';
 import {SessionManager} from '../Models/Sessions/SessionManager';
 import SettingItem from '../Components/Views/SettingItem';
+import {useDispatch} from 'react-redux';
+import {AppActions} from '../Redux/Actions/AppActions';
 
 const Settings = props => {
+  const dispatch = useDispatch();
   const handleLogout = () => {
-    SessionManager.clear();
+    dispatch({type: AppActions.LOGOUT});
     props.navigation.reset({
       index: 0,
       routes: [{name: 'Login'}],
