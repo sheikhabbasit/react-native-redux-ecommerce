9154f262e4a3a5a3838c71b53906795aa48c2013
sheikhabbasit <sheikh.abdul.basit95@gmail.com>
2022-02-24 12:24:50 +0530

    style(HomeStackNavigator): add icon to header

--- Source/Navigation/HomeStackNavigator.js
+++ Source/Navigation/HomeStackNavigator.js
@@ -2,13 +2,26 @@ import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Home from '../Screens/Home';
 import ProductDetails from '../Screens/ProductDetails';
-
+import Ionicons from 'react-native-vector-icons/Ionicons';
 const Stack = createNativeStackNavigator();
 
 const SettingsStackNavigator = () => {
   return (
     <Stack.Navigator initialRouteName="FoodKhana">
-      <Stack.Screen name="FoodKhana" component={Home} />
+      <Stack.Screen
+        name="FoodKhana"
+        component={Home}
+        options={{
+          headerLeft: props => (
+            <Ionicons
+              name="fast-food"
+              size={30}
+              color="#9C0F48"
+              style={{marginEnd: 10}}
+            />
+          ),
+        }}
+      />
       <Stack.Screen name="Product Details" component={ProductDetails} />
     </Stack.Navigator>
   );
