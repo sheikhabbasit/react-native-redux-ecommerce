3d7594464ea9ab99eec04e71d0c1a5107016dc74
sheikhabbasit <sheikh.abdul.basit95@gmail.com>
2022-02-24 12:25:15 +0530

    feat(SettingsStackNaigator): add icon to header

--- Source/Navigation/SettingsStackNavigator.js
+++ Source/Navigation/SettingsStackNavigator.js
@@ -2,11 +2,23 @@ import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Settings from '../Screens/Settings';
 import EditCredentials from '../Screens/EditCredentials';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 const Stack = createNativeStackNavigator();
 
 const SettingsStackNavigator = () => {
   return (
-    <Stack.Navigator initialRouteName="Settings">
+    <Stack.Navigator
+      initialRouteName="Settings"
+      screenOptions={{
+        headerLeft: props => (
+          <Ionicons
+            name="settings"
+            size={30}
+            color="#9C0F48"
+            style={{marginEnd: 10}}
+          />
+        ),
+      }}>
       <Stack.Screen name="Setting" component={Settings} />
       <Stack.Screen name="EditCredentials" component={EditCredentials} />
     </Stack.Navigator>
