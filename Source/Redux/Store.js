b3ed4e86bc02833e9832e6b30ce182059c028b5c
sheikhabbasit <sheikh.abdul.basit95@gmail.com>
2022-02-24 18:09:47 +0530

    feat(Store.js): add cart reducer to store

--- Source/Redux/Store.js
+++ Source/Redux/Store.js
@@ -1,17 +1,19 @@
 import {combineReducers, createStore} from 'redux';
 import {AppReducer} from './Reducer/AppReducer';
+import {CartReducer} from './Reducer/CartReducer';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import persistReducer from 'redux-persist/es/persistReducer';
 import persistStore from 'redux-persist/es/persistStore';
 
 const AllReducers = {
   app: AppReducer,
+  cart: CartReducer,
 };
 
 const PersistConfig = {
   key: 'root',
   storage: AsyncStorage,
-  whitelist: ['app'],
+  whitelist: ['app', 'cart'],
 };
 const mergedReducer = combineReducers(AllReducers);
 const persistedReducer = persistReducer(PersistConfig, mergedReducer);
