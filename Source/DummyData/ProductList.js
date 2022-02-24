c937d0d247fb78999e579c37e6f4f653e3f787b4
sheikhabbasit <sheikh.abdul.basit95@gmail.com>
2022-02-24 11:33:42 +0530

    feat(ProductList): Add products json and images

---
+++ Source/DummyData/ProductList.js
@@ -0,0 +1,32 @@
+export const Products = [
+  {
+    id: 1,
+    name: 'Product 1',
+    price: '$100',
+    imageSource: require('../Resources/Images/product1.jpg'),
+  },
+  {
+    id: 2,
+    name: 'Product 2',
+    price: '$200',
+    imageSource: require('../Resources/Images/product2.jpg'),
+  },
+  {
+    id: 3,
+    name: 'Product 3',
+    price: '$300',
+    imageSource: require('../Resources/Images/product3.jpg'),
+  },
+  {
+    id: 4,
+    name: 'Product 4',
+    price: '$400',
+    imageSource: require('../Resources/Images/product4.jpg'),
+  },
+  {
+    id: 5,
+    name: 'Product 5',
+    price: '$500',
+    imageSource: require('../Resources/Images/product5.jpg'),
+  },
+];
