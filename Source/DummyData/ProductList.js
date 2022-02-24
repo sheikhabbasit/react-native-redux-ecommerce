58af584909a91530bcf7505d1a442a9577aee575
sheikhabbasit <sheikh.abdul.basit95@gmail.com>
2022-02-24 13:44:53 +0530

    feat(ProdutList): add description to products

--- Source/DummyData/ProductList.js
+++ Source/DummyData/ProductList.js
@@ -5,6 +5,8 @@ export const Products = [
     price: '$100',
     imageSource: require('../Resources/Images/product1.jpg'),
     discount: '10%',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
   },
   {
     id: 2,
@@ -12,6 +14,8 @@ export const Products = [
     price: '$200',
     imageSource: require('../Resources/Images/product2.jpg'),
     discount: '20%',
+    description:
+      'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.',
   },
   {
     id: 3,
@@ -19,6 +23,8 @@ export const Products = [
     price: '$300',
     imageSource: require('../Resources/Images/product3.jpg'),
     discount: '30%',
+    description:
+      'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?',
   },
   {
     id: 4,
@@ -26,6 +32,8 @@ export const Products = [
     price: '$400',
     imageSource: require('../Resources/Images/product4.jpg'),
     discount: '40%',
+    description:
+      'Ut efficitur neque eget neque lobortis, et pellentesque ex pulvinar. Sed feugiat diam eu viverra bibendum. Quisque lobortis, leo a fermentum eleifend, quam lectus molestie ante, et egestas lectus ipsum quis ex. Integer varius velit ac magna finibus eleifend. Etiam vitae odio quam. Morbi interdum mattis placerat. Integer et lectus ut massa hendrerit facilisis. Nam eget ex vitae elit vehicula eleifend sit amet at urna. Nullam felis erat, dictum eu accumsan vel, facilisis sed lorem. Curabitur et nibh ultricies, sodales sapien vel, euismod lectus. Sed nec facilisis justo, id dictum dui. Vestibulum consectetur consequat turpis, molestie egestas turpis. Integer mattis semper orci, pellentesque consectetur tortor dictum a. Curabitur mauris neque, posuere id tortor vitae, vulputate efficitur velit.',
   },
   {
     id: 5,
@@ -33,5 +41,7 @@ export const Products = [
     price: '$500',
     imageSource: require('../Resources/Images/product5.jpg'),
     discount: '50%',
+    description:
+      'Nunc convallis, sapien vel commodo condimentum, nisl turpis ullamcorper dolor, laoreet condimentum augue dui sed enim. Fusce vulputate, nulla iaculis aliquam tincidunt, neque leo hendrerit turpis, non condimentum tellus felis eu leo. Integer ac ornare ipsum. Nullam vel mauris nisl. Praesent ac pharetra orci. Maecenas vulputate diam justo, a tincidunt augue iaculis vitae. Praesent bibendum lorem at lectus tempor, eu accumsan ex tempus. Proin maximus erat elit, nec interdum massa placerat in. Ut hendrerit, lectus ac congue lobortis, ipsum ipsum egestas ex, a luctus ex turpis non nulla. Suspendisse eu dapibus odio, non rutrum mi. Sed id nunc id magna dignissim ullamcorper. Proin euismod nulla ante. Praesent dui tellus, dignissim ut lorem eu, tempor volutpat leo. Suspendisse potenti.',
   },
 ];
