7ca191807ef836871907fcb51585b56fc1b576a9
sheikhabbasit <sheikh.abdul.basit95@gmail.com>
2022-02-24 11:34:38 +0530

    feat(ProductItem): New component to dsiplay items on home screen

--- Source/Components/Views/ProductItem.js
+++ Source/Components/Views/ProductItem.js
@@ -1,12 +1,70 @@
-import {View, Text} from 'react-native';
+import {Image, Text, View, StyleSheet, Pressable} from 'react-native';
 import React from 'react';
+import Card from '../HOC/Card';
+import {useNavigation} from '@react-navigation/native';
 
 const ProductItem = props => {
+  const {imageSource, name, price} = props.product;
+  const {product} = props;
+  const navigation = useNavigation();
+
+  const navigateTo = () => {
+    navigation.navigate('Product Details', {product});
+  };
+
   return (
-    <View>
-      <Text>ProductItem</Text>
-    </View>
+    <Card>
+      <Pressable onPress={navigateTo}>
+        <View style={styles.container}>
+          <Image style={styles.image} source={imageSource} />
+          <View style={styles.detailsContainer}>
+            <Text style={styles.productLabel}>{name}</Text>
+            <Text style={styles.productLabel}>{price}</Text>
+          </View>
+        </View>
+        <Pressable
+          onPress={navigateTo}
+          style={styles.cartButton}
+          android_ripple={{color: 'white'}}>
+          <Text style={styles.buttonLabel}>Details</Text>
+        </Pressable>
+      </Pressable>
+    </Card>
   );
 };
 
 export default ProductItem;
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  },
+  detailsContainer: {
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  productLabel: {
+    fontSize: 15,
+    color: '#FFBBBB',
+    fontWeight: 'normal',
+  },
+  image: {
+    width: 150,
+    height: 150,
+    borderRadius: 10,
+  },
+  cartButton: {
+    marginTop: 20,
+    padding: 10,
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FFBBBB',
+  },
+  buttonLabel: {
+    fontWeight: 'bold',
+    color: '#9C0F48',
+  },
+});
