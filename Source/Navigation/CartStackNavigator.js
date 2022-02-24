677eb98fd82088102f15bf945a8cd947036e7fd8
sheikhabbasit <sheikh.abdul.basit95@gmail.com>
2022-02-24 12:24:09 +0530

    style(CartStackNavigator): add icon to header

--- Source/Navigation/CartStackNavigator.js
+++ Source/Navigation/CartStackNavigator.js
@@ -2,13 +2,27 @@ import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import ProductDetails from '../Screens/ProductDetails';
 import Cart from '../Screens/Cart';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const Stack = createNativeStackNavigator();
 
 const CartStackNavigator = () => {
   return (
     <Stack.Navigator initialRouteName="Cart">
-      <Stack.Screen name="Cart " component={Cart} />
+      <Stack.Screen
+        name="Cart Tanker"
+        component={Cart}
+        options={{
+          headerLeft: props => (
+            <Ionicons
+              name="cart"
+              size={30}
+              color="#9C0F48"
+              style={{marginEnd: 10}}
+            />
+          ),
+        }}
+      />
       <Stack.Screen name="Product Details" component={ProductDetails} />
     </Stack.Navigator>
   );
