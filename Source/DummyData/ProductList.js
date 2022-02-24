50f1ec5960e8c61f8bce3da92eb8102caed7430d
sheikhabbasit <sheikh.abdul.basit95@gmail.com>
2022-02-24 12:23:36 +0530

    feat(ProductList): add discount property to json file

--- Source/DummyData/ProductList.js
+++ Source/DummyData/ProductList.js
@@ -1,32 +1,37 @@
 export const Products = [
   {
     id: 1,
-    name: 'Product 1',
+    name: 'Yummy Burger',
     price: '$100',
     imageSource: require('../Resources/Images/product1.jpg'),
+    discount: '10%',
   },
   {
     id: 2,
-    name: 'Product 2',
+    name: 'The Berry Pastry',
     price: '$200',
     imageSource: require('../Resources/Images/product2.jpg'),
+    discount: '20%',
   },
   {
     id: 3,
-    name: 'Product 3',
+    name: 'Black Sandwich',
     price: '$300',
     imageSource: require('../Resources/Images/product3.jpg'),
+    discount: '30%',
   },
   {
     id: 4,
-    name: 'Product 4',
+    name: 'Popular Pizza',
     price: '$400',
     imageSource: require('../Resources/Images/product4.jpg'),
+    discount: '40%',
   },
   {
     id: 5,
-    name: 'Product 5',
+    name: 'FoodKhana Special',
     price: '$500',
     imageSource: require('../Resources/Images/product5.jpg'),
+    discount: '50%',
   },
 ];
