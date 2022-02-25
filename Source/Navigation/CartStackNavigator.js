57304c8344849c68a51be9b7ab9a661637d0a097
sheikhabbasit <sheikh.abdul.basit95@gmail.com>
2022-02-25 13:04:28 +0530

    style(CartStackNavigator): add header color

--- Source/Navigation/CartStackNavigator.js
+++ Source/Navigation/CartStackNavigator.js
@@ -10,17 +10,25 @@ const CartStackNavigator = () => {
   return (
     <Stack.Navigator initialRouteName="Cart">
       <Stack.Screen
-        name="Cart Tanker"
+        name="CartPage"
         component={Cart}
         options={{
+          headerTintColor: '#eda6c2',
+          headerStyle: {
+            backgroundColor: '#9C0F48',
+          },
+          headerTitleStyle: {
+            fontWeight: '700',
+          },
           headerLeft: props => (
             <Ionicons
               name="cart"
               size={30}
-              color="#9C0F48"
+              color="#eda6c2"
               style={{marginEnd: 10}}
             />
           ),
+          title: 'Cart',
         }}
       />
       <Stack.Screen name="Product Details" component={ProductDetails} />
