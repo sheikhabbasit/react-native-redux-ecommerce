bec43a380c29d6185b2c550f7cc3f6d2aeb38784
sheikhabbasit <sheikh.abdul.basit95@gmail.com>
2022-02-25 13:06:08 +0530

    style(SettingsStackNavigator): add header color, icons, and go-back arrow icon

--- Source/Navigation/SettingsStackNavigator.js
+++ Source/Navigation/SettingsStackNavigator.js
@@ -3,24 +3,57 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Settings from '../Screens/Settings';
 import EditCredentials from '../Screens/EditCredentials';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import {useNavigation} from '@react-navigation/native';
+
 const Stack = createNativeStackNavigator();
 
 const SettingsStackNavigator = () => {
+  const navigation = useNavigation();
   return (
-    <Stack.Navigator
-      initialRouteName="Settings"
-      screenOptions={{
-        headerLeft: props => (
-          <Ionicons
-            name="settings"
-            size={30}
-            color="#9C0F48"
-            style={{marginEnd: 10}}
-          />
-        ),
-      }}>
-      <Stack.Screen name="Settings" component={Settings} />
-      <Stack.Screen name="EditCredentials" component={EditCredentials} />
+    <Stack.Navigator initialRouteName="Settings">
+      <Stack.Screen
+        name="Settings"
+        component={Settings}
+        options={{
+          headerTintColor: '#eda6c2',
+          headerStyle: {
+            backgroundColor: '#9C0F48',
+          },
+          headerTitleStyle: {
+            fontWeight: '700',
+          },
+          headerLeft: props => (
+            <Ionicons
+              name="settings"
+              size={30}
+              color="#eda6c2"
+              style={{marginEnd: 10}}
+            />
+          ),
+        }}
+      />
+      <Stack.Screen
+        name="EditCredentials"
+        component={EditCredentials}
+        options={{
+          headerTintColor: '#eda6c2',
+          headerStyle: {
+            backgroundColor: '#9C0F48',
+          },
+          headerTitleStyle: {
+            fontWeight: '700',
+          },
+          headerLeft: props => (
+            <Ionicons
+              name="arrow-back-circle-outline"
+              size={30}
+              color="#eda6c2"
+              style={{marginEnd: 10}}
+              onPress={() => navigation.navigate('Settings')}
+            />
+          ),
+        }}
+      />
     </Stack.Navigator>
   );
 };
