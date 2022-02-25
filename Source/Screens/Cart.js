175b950b459299c41da597b8565c80038480a7b6
sheikhabbasit <sheikh.abdul.basit95@gmail.com>
2022-02-25 17:32:51 +0530

    feat(CartItem.js): add flatlist to display cart data

--- Source/Screens/Cart.js
+++ Source/Screens/Cart.js
@@ -1,21 +1,70 @@
-import {Text, SafeAreaView, ScrollView, StyleSheet} from 'react-native';
-import React, {useState} from 'react';
+import {
+  FlatList,
+  View,
+  Text,
+  SafeAreaView,
+  StyleSheet,
+  TextInput,
+  ScrollView,
+} from 'react-native';
+import React from 'react';
+import CartItem from '../Components/Views/CartItem';
+import {useSelector} from 'react-redux';
 
 const Cart = props => {
+  const {cartItems, idWithQuantity} = useSelector(state => state.cart);
+
   return (
-    <SafeAreaView style={styles.parent}>
-      <ScrollView contentContainerStyle={styles.card}></ScrollView>
+    <SafeAreaView style={styles.wrapper}>
+      <FlatList
+        contentContainerStyle={styles.list}
+        data={cartItems}
+        renderItem={({item}) => (
+          <CartItem product={item} idWithQuantity={idWithQuantity} />
+        )}
+        keyExtractor={item => item.id}
+      />
+      <ScrollView style={styles.checkoutParent}>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Enter delivery location"
+        />
+        <View style={styles.checkoutContainer}>
+          <Text>Checkout</Text>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
 
-export default Cart;
-
 const styles = StyleSheet.create({
-  parent: {
-    flex: 1,
+  wrapper: {
     backgroundColor: '#FFCBCB',
-    justifyContent: 'center',
-    alignItems: 'center',
+    flex: 1,
+  },
+  list: {
+    borderRadius: 10,
+    padding: 20,
+  },
+  textInput: {
+    borderRadius: 10,
+    backgroundColor: '#fff',
+    marginBottom: 10,
+  },
+  checkoutParent: {
+    borderTopWidth: 3,
+    borderTopColor: '#eda6c2',
+    backgroundColor: '#9C0F48',
+    padding: 20,
+    height: '50%',
+    marginTop: 10,
+  },
+  checkoutContainer: {
+    flexDirection: 'row',
+    backgroundColor: '#9C0F48',
+    justifyContent: 'flex-end',
+    borderRadius: 10,
   },
 });
+
+export default Cart;
