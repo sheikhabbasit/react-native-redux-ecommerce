9642436421c26503bee3bfa5bcddd3f02e06ee4d
sheikhabbasit <sheikh.abdul.basit95@gmail.com>
2022-02-28 18:00:39 +0530

    feat(Home.js): Add search facility

--- Source/Screens/Home.js
+++ Source/Screens/Home.js
@@ -1,24 +1,43 @@
 import {FlatList, SafeAreaView, StyleSheet, TextInput} from 'react-native';
-import React, {useLayoutEffect} from 'react';
+import React, {useLayoutEffect, useRef, useState} from 'react';
 import ProductItem from '../Components/Views/ProductItem';
 import {Products} from '../DummyData/ProductList';
 import {useNavigation} from '@react-navigation/native';
 import {useSelector} from 'react-redux';
 
 const Home = props => {
+  const [searchValue, setSearchValue] = useState('');
   const navigation = useNavigation();
   const {email} = useSelector(state => state.app.userInfo);
+  const [productList, setProductList] = useState(Products);
 
   useLayoutEffect(() => {
-    navigation.setOptions({title: `Welcome "${email}"`});
+    const fullName = email.split('@')[0];
+    const name = fullName[0].toUpperCase() + fullName.slice(1);
+    navigation.setOptions({title: `Welcome ${name}`});
   });
+
+  const handleChange = e => {
+    setSearchValue(e.nativeEvent.text);
+    if (e.nativeEvent.text.length === 0) return setProductList(Products);
+    setProductList(
+      Products.filter(product =>
+        product.name.toLowerCase().includes(e.nativeEvent.text),
+      ),
+    );
+  };
 
   return (
     <SafeAreaView style={styles.wrapper}>
-      <TextInput style={styles.textInput} placeholder="Search" />
+      <TextInput
+        style={styles.textInput}
+        placeholder="Search"
+        onChange={handleChange}
+        value={searchValue}
+      />
       <FlatList
         contentContainerStyle={styles.list}
-        data={Products}
+        data={productList}
         renderItem={({item}) => <ProductItem product={item} />}
         keyExtractor={item => item.id}
       />
@@ -33,14 +52,14 @@ const styles = StyleSheet.create({
   },
   textInput: {
     backgroundColor: '#fff',
-    marginTop: 10,
-    marginHorizontal: 10,
+    margin: 10,
     borderRadius: 10,
     padding: 10,
   },
   list: {
     borderRadius: 10,
     padding: 10,
+    paddingTop: 0,
   },
 });
 
