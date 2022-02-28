ab87eff6268a51be1730a78c7a61f5fca20bad5b
sheikhabbasit <sheikh.abdul.basit95@gmail.com>
2022-02-28 18:18:58 +0530

    feat(Cart.js): add conditional rendering of Cart Total values

--- Source/Screens/Cart.js
+++ Source/Screens/Cart.js
@@ -8,7 +8,7 @@ import {
   ScrollView,
   Pressable,
 } from 'react-native';
-import React, {Fragment, useLayoutEffect} from 'react';
+import React, {Fragment, useLayoutEffect, useState} from 'react';
 import CartItem from '../Components/Views/CartItem';
 import {useSelector} from 'react-redux';
 import Card from '../Components/HOC/Card';
@@ -16,8 +16,13 @@ import CartTotals from '../Components/Views/CartTotals';
 
 const Cart = props => {
   const {cartItems, idWithQuantity} = useSelector(state => state.cart);
-
+  const [listEmpty, setListEmpty] = useState(false);
   const cart = useSelector(state => state.cart);
+
+  useLayoutEffect(() => {
+    if (cartItems.length === 0) setListEmpty(true);
+    if (cartItems.length > 0) setListEmpty(false);
+  }, [cartItems]);
 
   const cartTotalValue =
     cart.cartItems?.reduce(
@@ -38,28 +43,33 @@ const Cart = props => {
           <CartItem product={item} idWithQuantity={idWithQuantity} />
         )}
         keyExtractor={item => item.id}
+        ListEmptyComponent={
+          <Text style={styles.emptyCart}>Add something to cart!</Text>
+        }
         ListFooterComponent={
-          <Fragment>
-            <Card>
-              <TextInput
-                style={styles.textInput}
-                placeholder="Enter delivery location"
-              />
-              <Text style={styles.generalText}>
-                <Text style={styles.boldText}>Destination: </Text>Dummy Selected
-                Location
-              </Text>
-            </Card>
-            <Card>
-              <CartTotals label="Cart Total:" amount={cartTotalValue} />
-              <CartTotals label="Shipping:" amount={shipping} />
-              <CartTotals label="Tax:" amount={tax} />
-              <CartTotals label="Amount Payable:" amount={amountPayable} />
-            </Card>
-            <Pressable style={styles.checkoutButton}>
-              <Text style={styles.checkoutLabel}>Checkout</Text>
-            </Pressable>
-          </Fragment>
+          !listEmpty && (
+            <Fragment>
+              <Card>
+                <TextInput
+                  style={styles.textInput}
+                  placeholder="Enter delivery location"
+                />
+                <Text style={styles.generalText}>
+                  <Text style={styles.boldText}>Destination: </Text>Dummy
+                  Selected Location
+                </Text>
+              </Card>
+              <Card>
+                <CartTotals label="Cart Total:" amount={cartTotalValue} />
+                <CartTotals label="Shipping:" amount={shipping} />
+                <CartTotals label="Tax:" amount={tax} />
+                <CartTotals label="Amount Payable:" amount={amountPayable} />
+              </Card>
+              <Pressable style={styles.checkoutButton}>
+                <Text style={styles.checkoutLabel}>Checkout</Text>
+              </Pressable>
+            </Fragment>
+          )
         }
       />
     </SafeAreaView>
@@ -91,6 +101,12 @@ const styles = StyleSheet.create({
     backgroundColor: '#9C0F48',
     justifyContent: 'flex-end',
     borderRadius: 10,
+  },
+  emptyCart: {
+    fontSize: 20,
+    textAlign: 'center',
+    color: '#9C0F48',
+    margin: 20,
   },
   boldText: {
     fontWeight: 'bold',
