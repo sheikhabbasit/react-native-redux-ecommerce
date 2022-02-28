63c46c12caef11bc15dbff89d92669be108960e2
sheikhabbasit <sheikh.abdul.basit95@gmail.com>
2022-02-28 11:59:05 +0530

    feat(Home.js): add search bar

--- Source/Screens/Home.js
+++ Source/Screens/Home.js
@@ -1,4 +1,4 @@
-import {FlatList, SafeAreaView, StyleSheet} from 'react-native';
+import {FlatList, SafeAreaView, StyleSheet, TextInput} from 'react-native';
 import React from 'react';
 import ProductItem from '../Components/Views/ProductItem';
 import {Products} from '../DummyData/ProductList';
@@ -6,6 +6,7 @@ import {Products} from '../DummyData/ProductList';
 const Home = props => {
   return (
     <SafeAreaView style={styles.wrapper}>
+      <TextInput style={styles.textInput} placeholder="Search" />
       <FlatList
         contentContainerStyle={styles.list}
         data={Products}
@@ -20,6 +21,13 @@ const styles = StyleSheet.create({
   wrapper: {
     backgroundColor: '#FFCBCB',
     flex: 1,
+  },
+  textInput: {
+    backgroundColor: '#fff',
+    marginTop: 10,
+    marginHorizontal: 10,
+    borderRadius: 10,
+    padding: 10,
   },
   list: {
     borderRadius: 10,
