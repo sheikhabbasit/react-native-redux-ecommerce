c31ed1c402528525154c1d52372072ef0f237c9d
sheikhabbasit <sheikh.abdul.basit95@gmail.com>
2022-03-02 15:56:41 +0530

    feat(SettingItem): add android ripple

--- Source/Components/Views/SettingItem.js
+++ Source/Components/Views/SettingItem.js
@@ -1,24 +1,36 @@
-import {Text, Pressable, StyleSheet} from 'react-native';
+import {Text, View, Pressable, StyleSheet} from 'react-native';
 import React from 'react';
 
 const SettingItem = props => {
   const {onPress, label} = props;
   return (
-    <Pressable style={styles.button} onPress={onPress}>
-      <Text style={styles.textLabel}>{label}</Text>
-    </Pressable>
+    <View style={styles.buttonContainer}>
+      <Pressable
+        android_ripple={{color: 'white'}}
+        style={styles.button}
+        onPress={onPress}>
+        <Text style={styles.textLabel}>{label}</Text>
+      </Pressable>
+    </View>
   );
 };
 
 export default SettingItem;
 
 const styles = StyleSheet.create({
+  buttonContainer: {
+    alignSelf: 'stretch',
+    borderRadius: 10,
+    elevation: 20,
+    overflow: 'hidden',
+    borderRadius: 5,
+    marginHorizontal: 10,
+    marginTop: 5,
+    borderRadius: 10,
+  },
   button: {
     backgroundColor: '#9C0F48',
     padding: 15,
-    borderRadius: 15,
-    marginHorizontal: 10,
-    marginTop: 5,
   },
   textLabel: {
     color: '#fff',
