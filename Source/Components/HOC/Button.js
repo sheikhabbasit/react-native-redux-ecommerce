56649fe2518c9a9a032f8f0de88f8a17cd20a465
sheikhabbasit <sheikh.abdul.basit95@gmail.com>
2022-03-02 15:55:00 +0530

    fix(Button.js): fix android ripple overflow

--- Source/Components/HOC/Button.js
+++ Source/Components/HOC/Button.js
@@ -1,31 +1,38 @@
-import {Pressable, Text, StyleSheet} from 'react-native';
+import {Pressable, Text, View, StyleSheet} from 'react-native';
 import React from 'react';
 
 const Button = ({onPress, label}) => {
   return (
-    <Pressable
-      onPress={onPress}
-      style={styles.cartButton}
-      android_ripple={{color: 'white'}}>
-      <Text style={styles.buttonLabel}>{label}</Text>
-    </Pressable>
+    <View style={styles.buttonContainer}>
+      <Pressable
+        onPress={onPress}
+        style={styles.cartButton}
+        android_ripple={{color: 'white', borderless: true}}>
+        <Text style={styles.buttonLabel}>{label}</Text>
+      </Pressable>
+    </View>
   );
 };
 
 export default Button;
 
 const styles = StyleSheet.create({
-  buttonLabel: {
-    fontWeight: 'bold',
-    color: '#9C0F48',
+  buttonContainer: {
+    alignSelf: 'stretch',
+    justifyContent: 'center',
+    borderRadius: 10,
+    margin: 10,
+    elevation: 25,
   },
   cartButton: {
-    marginTop: 10,
     padding: 10,
-    borderRadius: 10,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#FFBBBB',
-    elevation: 10,
+    height: 40,
+  },
+  buttonLabel: {
+    fontWeight: 'bold',
+    color: '#9C0F48',
   },
 });
