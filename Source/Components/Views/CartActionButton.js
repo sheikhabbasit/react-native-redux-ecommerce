5b2326f4cfbd364d13fed1ba84521226762d89ce
sheikhabbasit <sheikh.abdul.basit95@gmail.com>
2022-03-02 15:55:32 +0530

    fix(CartActionButton): fix android ripple overflow

--- Source/Components/Views/CartActionButton.js
+++ Source/Components/Views/CartActionButton.js
@@ -1,28 +1,36 @@
-import {Text, Pressable, StyleSheet} from 'react-native';
+import {Text, Pressable, View, StyleSheet} from 'react-native';
 import React from 'react';
 
 const CartActionButton = ({onPress, label, extraMargin = false}) => {
   return (
-    <Pressable
-      onPress={onPress}
-      android_ripple={{color: 'white'}}
-      style={[styles.cartButton, extraMargin && styles.extraMargin]}>
-      <Text style={styles.buttonLabel}>{label}</Text>
-    </Pressable>
+    <View style={styles.buttonContainer}>
+      <Pressable
+        onPress={onPress}
+        android_ripple={{color: 'white', borderless: false}}
+        style={[styles.cartButton, extraMargin && styles.extraMargin]}>
+        <Text style={styles.buttonLabel}>{label}</Text>
+      </Pressable>
+    </View>
   );
 };
 
 export default CartActionButton;
 
 const styles = StyleSheet.create({
+  buttonContainer: {
+    alignSelf: 'stretch',
+    borderRadius: 10,
+    // elevation: 20,
+    overflow: 'hidden',
+    borderRadius: 5,
+  },
   cartButton: {
-    height: 25,
+    height: 30,
     width: 25,
     backgroundColor: '#eda6c2',
     borderRadius: 5,
     justifyContent: 'center',
     alignItems: 'center',
-    marginEnd: 10,
   },
   buttonLabel: {
     fontSize: 18,
