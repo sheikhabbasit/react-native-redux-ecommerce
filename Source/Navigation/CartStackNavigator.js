0d85be958144573fdb7b85b41b8d42ac6a4825b2
sheikhabbasit <sheikh.abdul.basit95@gmail.com>
2022-03-02 15:57:28 +0530

    feat(CartStackNavigator): add ability to go back to cart from product detail page

--- Source/Navigation/CartStackNavigator.js
+++ Source/Navigation/CartStackNavigator.js
@@ -3,10 +3,13 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import ProductDetails from '../Screens/ProductDetails';
 import Cart from '../Screens/Cart';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import {useNavigation} from '@react-navigation/native';
 
 const Stack = createNativeStackNavigator();
 
 const CartStackNavigator = () => {
+  const navigation = useNavigation();
+
   return (
     <Stack.Navigator initialRouteName="Cart">
       <Stack.Screen
@@ -31,7 +34,28 @@ const CartStackNavigator = () => {
           title: 'Cart',
         }}
       />
-      <Stack.Screen name="Product Details" component={ProductDetails} />
+      <Stack.Screen
+        name="Product Details"
+        component={ProductDetails}
+        options={{
+          headerTintColor: '#eda6c2',
+          headerStyle: {
+            backgroundColor: '#9C0F48',
+          },
+          headerTitleStyle: {
+            fontWeight: '700',
+          },
+          headerLeft: props => (
+            <Ionicons
+              name="arrow-back-circle-outline"
+              size={30}
+              color="#eda6c2"
+              style={{marginEnd: 10}}
+              onPress={() => navigation.navigate('CartPage')}
+            />
+          ),
+        }}
+      />
     </Stack.Navigator>
   );
 };
