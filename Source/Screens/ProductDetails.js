0f5ef4013821e9dcbcf8acdb828432bf6c2f3d21
sheikhabbasit <sheikh.abdul.basit95@gmail.com>
2022-03-07 11:54:16 +0530

    feat( ProductDetails): Add new header, fix long header text

--- Source/Screens/ProductDetails.js
+++ Source/Screens/ProductDetails.js
@@ -1,4 +1,4 @@
-import {Text, StyleSheet, ScrollView, Image} from 'react-native';
+import {Text, StyleSheet, ScrollView, Image, Dimensions} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import React, {useLayoutEffect} from 'react';
 import Card from '../Components/HOC/Card';
@@ -6,7 +6,9 @@ import Button from '../Components/HOC/Button';
 import {useDispatch} from 'react-redux';
 import {CartActions} from '../Redux/Actions/CartActions';
 import {useNavigation} from '@react-navigation/native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
+const {height, width} = Dimensions.get('window');
 const ProductDetails = props => {
   const {product} = props.route.params;
   const dispatch = useDispatch();
@@ -14,10 +16,21 @@ const ProductDetails = props => {
 
   useLayoutEffect(() => {
     navigation.setOptions({
-      title:
-        product.name.length > 23
-          ? product.name.slice(0, 22) + '...'
-          : product.name,
+      title: '',
+      headerLeft: props => (
+        <React.Fragment>
+          <Ionicons
+            name="arrow-back-circle-outline"
+            size={30}
+            color="#eda6c2"
+            style={{marginEnd: 10}}
+            onPress={() => navigation.goBack()}
+          />
+          <Text numberOfLines={1} style={styles.header}>
+            {product.name}
+          </Text>
+        </React.Fragment>
+      ),
     });
   });
 
@@ -64,6 +77,12 @@ const styles = StyleSheet.create({
     height: 250,
     borderRadius: 10,
     opacity: 0.8,
+  },
+  header: {
+    fontSize: 18,
+    color: '#eda6c2',
+    width: width - 70,
+    overflow: 'hidden',
   },
   label: {
     color: 'white',
