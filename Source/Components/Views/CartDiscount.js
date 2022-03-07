8880a42ac0f99393e4c3b8cfef5bd39eaa1b77af
sheikhabbasit <sheikh.abdul.basit95@gmail.com>
2022-03-07 11:58:02 +0530

    feat(CartDiscount): add new component for applying discount

---
+++ Source/Components/Views/CartDiscount.js
@@ -0,0 +1,62 @@
+import {Text, TextInput, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import Card from '../HOC/Card';
+import Button from '../HOC/Button';
+
+const CartDiscount = ({
+  placeholder,
+  boldTextLabel,
+  textLabel,
+  handleDiscount,
+  discountActive,
+}) => {
+  const [redeemCode, setRedeemCode] = useState('');
+  const handleSubmit = () => {
+    if (redeemCode === '10OFF') {
+      console.log(redeemCode);
+      handleDiscount(true);
+    } else {
+      handleDiscount(false);
+    }
+  };
+  return (
+    <Card>
+      {!discountActive && (
+        <React.Fragment>
+          <Text style={styles.generalText}>
+            <Text style={styles.boldText}>{boldTextLabel}</Text>
+            {textLabel}
+          </Text>
+          <TextInput
+            value={redeemCode}
+            style={styles.textInput}
+            placeholder={placeholder}
+            onChangeText={text => setRedeemCode(text)}
+          />
+          <Button label="Apply" onPress={handleSubmit} />
+        </React.Fragment>
+      )}
+      {discountActive && (
+        <Text style={styles.generalText}>Discount Applied: 10%</Text>
+      )}
+    </Card>
+  );
+};
+
+export default CartDiscount;
+
+const styles = StyleSheet.create({
+  textInput: {
+    borderRadius: 5,
+    backgroundColor: '#fff',
+    marginVertical: 5,
+    padding: 10,
+  },
+  generalText: {
+    color: '#FFCBCB',
+  },
+  boldText: {
+    fontWeight: 'bold',
+    color: '#ffcbcb',
+  },
+});
