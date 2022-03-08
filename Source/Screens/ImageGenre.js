ea2fd034f6ea6fa93613b66795f4fdd5d984017b
sheikhabbasit <sheikh.abdul.basit95@gmail.com>
2022-03-08 13:52:01 +0530

    feat(ImageGenre): new screen for experimentnavigator

---
+++ Source/Screens/ImageGenre.js
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const ImageGenre = () => {
+  return (
+    <View>
+      <Text>ImageGenre</Text>
+    </View>
+  );
+};
+
+export default ImageGenre;
