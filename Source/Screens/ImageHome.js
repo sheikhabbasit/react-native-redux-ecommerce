32e349e84e45b1b555e80ee560550bf94c3508c7
sheikhabbasit <sheikh.abdul.basit95@gmail.com>
2022-03-08 17:20:49 +0530

    feat(ImageHome): add flatlist of images on api call, set refresh controls

--- Source/Screens/ImageHome.js
+++ Source/Screens/ImageHome.js
@@ -1,14 +1,94 @@
 import React, {useState, useEffect} from 'react';
-import {View, Text} from 'react-native';
+import {
+  View,
+  Text,
+  FlatList,
+  RefreshControl,
+  Alert,
+  Image,
+  StyleSheet,
+  ActivityIndicator,
+} from 'react-native';
+import {getRandomDog} from '../Network/APIRequest';
 
 const ImageHome = props => {
   const [imageList, setImageList] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [refreshing, setRefreshing] = useState(false);
+
+  useEffect(() => {
+    getDogData();
+  }, []);
+
+  const getDogData = async () => {
+    setRefreshing(true);
+    imageList.length === 0 ? setLoading(true) : setLoading(false);
+    const res = await getRandomDog(40);
+    if (res.status === 'success') {
+      setImageList(res.message);
+    } else {
+      Alert.alert(
+        'Error',
+        'Something went wrong',
+        [{text: 'OK'}, {text: 'Cancel', onPress: () => getDogData()}],
+        {cancelable: false},
+      );
+    }
+    setRefreshing(false);
+    setLoading(false);
+  };
+
   return (
-    <View>
-      <Text>ImageHome</Text>
+    <View style={styles.wrapper}>
+      {loading && (
+        <View style={styles.loadingView}>
+          <ActivityIndicator size={'large'} />
+        </View>
+      )}
+      {!loading && imageList.length > 0 && (
+        <React.Fragment>
+          <FlatList
+            data={imageList}
+            renderItem={({item}) => {
+              return (
+                <View style={styles.imageCard}>
+                  <Image style={styles.image} source={{uri: item}} />
+                </View>
+              );
+            }}
+            keyExtractor={item => item.toString() + Math.random()}
+            numColumns={2}
+            showsVerticalScrollIndicator={false}
+            refreshControl={
+              <RefreshControl refreshing={refreshing} onRefresh={getDogData} />
+            }
+          />
+        </React.Fragment>
+      )}
     </View>
   );
 };
 
 export default ImageHome;
+
+const styles = StyleSheet.create({
+  wrapper: {
+    padding: 16,
+    flex: 1,
+  },
+  loadingView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  imageCard: {
+    margin: 10,
+    width: '45%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  image: {
+    height: 150,
+    width: 150,
+  },
+});
