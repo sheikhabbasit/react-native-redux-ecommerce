c8928b27329a25fe45ee6d5750eb93a60a0e93a1
sheikhabbasit <sheikh.abdul.basit95@gmail.com>
2022-03-10 13:31:15 +0530

    feat(CountryData): add countries data for section list

---
+++ Source/DummyData/CountryData.js
@@ -0,0 +1,46 @@
+export const DATA = [
+  {
+    title: 'A',
+    data: ['Azerbaijan', 'Arabia', 'Australia'],
+  },
+  {
+    title: 'B',
+    data: ['Bulgaria', 'Belgium', 'Bosnia and Herzegovina'],
+  },
+  {
+    title: 'C',
+    data: ['Czech Republic', 'Czechoslovakia', 'China'],
+  },
+  {
+    title: 'D',
+    data: ['Denmark', 'Djibouti'],
+  },
+  {
+    title: 'E',
+    data: ['Egypt', 'Estonia', 'Ethiopia'],
+  },
+  {
+    title: 'F',
+    data: ['Finland', 'Fiji', 'Falkland Islands'],
+  },
+];
+
+export const CountriesList = [
+  'Azerbaijan',
+  'Arabia',
+  'Australia',
+  'Bulgaria',
+  'Belgium',
+  'Bosnia and Herzegovina',
+  'Czech Republic',
+  'Czechoslovakia',
+  'China',
+  'Denmark',
+  'Djibouti',
+  'Egypt',
+  'Estonia',
+  'Ethiopia',
+  'Finland',
+  'Fiji',
+  'Falkland Islands',
+];
