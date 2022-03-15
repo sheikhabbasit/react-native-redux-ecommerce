1e1b92302da0162413923f0df2656f1dfcea4b7e
sheikhabbasit <sheikh.abdul.basit95@gmail.com>
2022-03-15 13:05:57 +0530

    feat(Store): add thunk to store

--- Source/Redux/Store.js
+++ Source/Redux/Store.js
@@ -1,4 +1,4 @@
-import {combineReducers, createStore} from 'redux';
+import {combineReducers, createStore, applyMiddleware, compose} from 'redux';
 import {AppReducer} from './Reducer/AppReducer';
 import {CartReducer} from './Reducer/CartReducer';
 import {ThemeReducer} from './Reducer/ThemeReducer';
@@ -6,6 +6,9 @@ import {ImagesReducer} from './Reducer/ImagesReducer';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import persistReducer from 'redux-persist/es/persistReducer';
 import persistStore from 'redux-persist/es/persistStore';
+import thunk from 'redux-thunk';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const AllReducers = {
   app: AppReducer,
@@ -21,5 +24,8 @@ const PersistConfig = {
 };
 const mergedReducer = combineReducers(AllReducers);
 const persistedReducer = persistReducer(PersistConfig, mergedReducer);
-export const store = createStore(persistedReducer);
+export const store = createStore(
+  persistedReducer,
+  composeEnhancers(applyMiddleware(thunk)),
+);
 export const persistor = persistStore(store);
