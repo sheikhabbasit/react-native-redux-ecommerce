ea10eb68be2422d69a1d32687c7a89d1fec0dec3
sheikhabbasit <sheikh.abdul.basit95@gmail.com>
2022-03-15 13:04:24 +0530

    refactor(ImagesReducer): remove unused code

--- Source/Redux/Reducer/ImagesReducer.js
+++ Source/Redux/Reducer/ImagesReducer.js
@@ -14,17 +14,3 @@ export const ImagesReducer = (state = {images: []}, payload) => {
       return initialState;
   }
 };
-
-// Action creator code
-
-// export const getImages = limit => {
-//   return async dispatch => {
-//     try {
-//       const response = await getRandomDog(limit);
-//       if (response.status === 'success')
-//         dispatch({type: ImagesActions.STORE_DATA, data: response.message});
-//     } catch (error) {
-//       console.log('Fetch failed', error);
-//     }
-//   };
-// };
