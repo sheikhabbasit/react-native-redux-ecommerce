f4b51e8f08296144107c5e89a2ad7b2207ed42a5
sheikhabbasit <sheikh.abdul.basit95@gmail.com>
2022-03-15 14:33:28 +0530

    refactor(ImageActionCreator): remove log

--- Source/Redux/ActionCreators/ImageActionCreator.js
+++ Source/Redux/ActionCreators/ImageActionCreator.js
@@ -4,7 +4,6 @@ import {getRandomDog} from '../../Network/APIRequest';
 export const fetchImagesList = () => {
   return async dispatch => {
     const response = await getRandomDog(20);
-    console.log('response: ', response);
     if (response.status === 'success') {
       dispatch({type: ImagesActions.STORE_DATA, payload: response.message});
     }
