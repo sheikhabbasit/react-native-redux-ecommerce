e64150dbdcfdd45dcc3a6139223b66f5058a2550
sheikhabbasit <sheikh.abdul.basit95@gmail.com>
2022-03-21 11:44:28 +0530

    feat(ImagesReducer): add two new methods for redux saga

--- Source/Redux/Reducer/ImagesReducer.js
+++ Source/Redux/Reducer/ImagesReducer.js
@@ -3,6 +3,14 @@ import {getRandomDog} from '../../Network/APIRequest';
 
 const initialState = {};
 
+export const getImages = (state = initialState, action) => ({
+  type: ImagesActions.GET_DATA,
+});
+
+export const storeImages = data => {
+  return {type: ImagesActions.STORE_DATA, payload: data};
+};
+
 export const ImagesReducer = (state = {images: []}, payload) => {
   switch (payload.type) {
     case ImagesActions.STORE_DATA:
