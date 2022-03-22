5d49e4836c0977aa77dfcf678992b13a6b27faf4
sheikhabbasit <sheikh.abdul.basit95@gmail.com>
2022-03-22 19:49:56 +0530

    feat(CountryData): new data for maps

--- Source/DummyData/CountryData.js
+++ Source/DummyData/CountryData.js
@@ -44,3 +44,20 @@ export const CountriesList = [
   'Fiji',
   'Falkland Islands',
 ];
+
+export const CountriesWithLocation = [
+  {name: 'Australia', code: 'AU', lat: -25.274398, lng: 133.775136},
+  {name: 'Austria', code: 'AT', lat: 47.516231, lng: 14.550072},
+  {name: 'Belgium', code: 'BE', lat: 50.503887, lng: 4.469936},
+  {name: 'Brazil', code: 'BR', lat: -15.780199, lng: -47.929509},
+  {name: 'Canada', code: 'CA', lat: 56.130366, lng: -106.346771},
+  {name: 'China', code: 'CN', lat: 35.86166, lng: 104.195397},
+  {name: 'Denmark', code: 'DK', lat: 56.26392, lng: 9.501785},
+  {name: 'Finland', code: 'FI', lat: 61.92411, lng: 25.748151},
+  {name: 'France', code: 'FR', lat: 46.227638, lng: 2.213749},
+  {name: 'Germany', code: 'DE', lat: 51.165691, lng: 10.451526},
+  {name: 'Hong Kong', code: 'HK', lat: 22.396428, lng: 114.109497},
+  {name: 'India', code: 'IN', lat: 20.593684, lng: 78.96288},
+  {name: 'Indonesia', code: 'ID', lat: -0.789275, lng: 113.921327},
+  {name: 'Ireland', code: 'IE', lat: 53.41291, lng: -8.24389},
+];
