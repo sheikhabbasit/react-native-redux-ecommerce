110003022dc1ba1c7d7798fa9a7301c6f456da70
sheikhabbasit <sheikh.abdul.basit95@gmail.com>
2022-03-24 20:28:12 +0530

    refactor(ExperimentalStackNavigator): use custom header

--- Source/Navigation/ExperimentalSpaceNavigator.js
+++ Source/Navigation/ExperimentalSpaceNavigator.js
@@ -10,12 +10,23 @@ import Pagination from '../Screens/Pagination';
 import {useTheme} from '../Hooks/useTheme';
 import Map from '../Screens/Map';
 import LocationSelect from '../Screens/LocationSelect';
+import Header from '../Components/Typography/Header';
 
 const Stack = createNativeStackNavigator();
 
 const ExperimentalStackNavigator = () => {
   const navigation = useNavigation();
   const darkMode = useTheme();
+  const headerStyles = {
+    headerTintColor: darkMode ? 'white' : '#eda6c2',
+    headerStyle: {
+      backgroundColor: darkMode ? '#1B1A17' : '#9C0F48',
+    },
+    headerTitleStyle: {
+      fontWeight: '700',
+    },
+    title: '',
+  };
 
   return (
     <Stack.Navigator initialRouteName="Image Home">
@@ -23,42 +34,32 @@ const ExperimentalStackNavigator = () => {
         name="Image Home"
         component={ImageHome}
         options={{
-          headerTintColor: darkMode ? 'white' : '#eda6c2',
-          headerStyle: {
-            backgroundColor: darkMode ? '#1B1A17' : '#9C0F48',
-          },
-          headerTitleStyle: {
-            fontWeight: '700',
-          },
+          ...headerStyles,
           headerLeft: props => (
-            <Ionicons
-              name="md-images"
+            <Header
+              iconName="md-images"
               size={30}
-              color={darkMode ? 'white' : '#eda6c2'}
-              style={{marginEnd: 10}}
+              theme={darkMode ? 'white' : '#eda6c2'}
               onPress={() => navigation.navigate('Image Genre')}
+              name={'Images'}
+              variant="Bold"
             />
           ),
-          title: 'Images',
         }}
       />
       <Stack.Screen
         name="Image Genre"
         component={ImageGenre}
         options={{
-          headerTintColor: '#eda6c2',
-          headerStyle: {
-            backgroundColor: '#9C0F48',
-          },
-          headerTitleStyle: {
-            fontWeight: '700',
-          },
+          ...headerStyles,
           headerLeft: props => (
-            <Ionicons
-              name="md-images"
+            <Header
+              iconName="arrow-back-circle-outline"
               size={30}
-              color="#eda6c2"
-              style={{marginEnd: 10}}
+              theme={darkMode}
+              onPress={() => navigation.navigate('Image Home')}
+              name={'Image Genre'}
+              variant="Bold"
             />
           ),
         }}
@@ -67,19 +68,15 @@ const ExperimentalStackNavigator = () => {
         name="Image With Breed"
         component={ImageWithBreed}
         options={{
-          headerTintColor: '#eda6c2',
-          headerStyle: {
-            backgroundColor: '#9C0F48',
-          },
-          headerTitleStyle: {
-            fontWeight: '700',
-          },
+          ...headerStyles,
           headerLeft: props => (
-            <Ionicons
-              name="md-images"
+            <Header
+              iconName="arrow-back-circle-outline"
               size={30}
-              color="#eda6c2"
-              style={{marginEnd: 10}}
+              theme={darkMode}
+              onPress={() => navigation.navigate('Image Home')}
+              name={'Images of Dog Breed'}
+              variant="Bold"
             />
           ),
         }}
@@ -88,19 +85,15 @@ const ExperimentalStackNavigator = () => {
         name="Countries"
         component={Countries}
         options={{
-          headerTintColor: '#eda6c2',
-          headerStyle: {
-            backgroundColor: '#9C0F48',
-          },
-          headerTitleStyle: {
-            fontWeight: '700',
-          },
+          ...headerStyles,
           headerLeft: props => (
-            <Ionicons
-              name="map"
+            <Header
+              iconName="arrow-back-circle-outline"
               size={30}
-              color="#eda6c2"
-              style={{marginEnd: 10}}
+              theme={darkMode}
+              onPress={() => navigation.navigate('Image Home')}
+              name={'Countries'}
+              variant="Bold"
             />
           ),
         }}
@@ -109,19 +102,15 @@ const ExperimentalStackNavigator = () => {
         name="Image With Pagination"
         component={Pagination}
         options={{
-          headerTintColor: '#eda6c2',
-          headerStyle: {
-            backgroundColor: '#9C0F48',
-          },
-          headerTitleStyle: {
-            fontWeight: '700',
-          },
+          ...headerStyles,
           headerLeft: props => (
-            <Ionicons
-              name="md-images"
+            <Header
+              iconName="arrow-back-circle-outline"
               size={30}
-              color="#eda6c2"
-              style={{marginEnd: 10}}
+              theme={darkMode}
+              onPress={() => navigation.navigate('Image Home')}
+              name={'Images With Pagination'}
+              variant="Bold"
             />
           ),
         }}
@@ -130,19 +119,15 @@ const ExperimentalStackNavigator = () => {
         name="Location Select"
         component={LocationSelect}
         options={{
-          headerTintColor: '#eda6c2',
-          headerStyle: {
-            backgroundColor: '#9C0F48',
-          },
-          headerTitleStyle: {
-            fontWeight: '700',
-          },
+          ...headerStyles,
           headerLeft: props => (
-            <Ionicons
-              name="md-map"
+            <Header
+              iconName="arrow-back-circle-outline"
               size={30}
-              color="#eda6c2"
-              style={{marginEnd: 10}}
+              theme={darkMode}
+              onPress={() => navigation.navigate('Image Home')}
+              name={'Countries'}
+              variant="Bold"
             />
           ),
         }}
@@ -151,19 +136,15 @@ const ExperimentalStackNavigator = () => {
         name="Map"
         component={Map}
         options={{
-          headerTintColor: '#eda6c2',
-          headerStyle: {
-            backgroundColor: '#9C0F48',
-          },
-          headerTitleStyle: {
-            fontWeight: '700',
-          },
+          ...headerStyles,
           headerLeft: props => (
-            <Ionicons
-              name="md-map"
+            <Header
+              iconName="arrow-back-circle-outline"
               size={30}
-              color="#eda6c2"
-              style={{marginEnd: 10}}
+              theme={darkMode ? 'white' : '#eda6c2'}
+              onPress={() => navigation.navigate('Image Home')}
+              name={'Maps'}
+              variant="Bold"
             />
           ),
         }}
