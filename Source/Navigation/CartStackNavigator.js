7cd0f46a7553e4957fb6ba5c423d0f0fc4aa8216
sheikhabbasit <sheikh.abdul.basit95@gmail.com>
2022-03-28 12:14:33 +0530

    refactor(CartStackNavigator): remove headerStyles object, minimum props

--- Source/Navigation/CartStackNavigator.js
+++ Source/Navigation/CartStackNavigator.js
@@ -2,10 +2,9 @@ import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import ProductDetails from '../Screens/ProductDetails';
 import Cart from '../Screens/Cart';
-import Ionicons from 'react-native-vector-icons/Ionicons';
 import {useNavigation} from '@react-navigation/native';
 import {useTheme} from '../Hooks/useTheme';
-import Header from '../Components/Typography/Header';
+import {headerReturn} from '../HelperFunctions/HeaderReturn';
 
 const Stack = createNativeStackNavigator();
 
@@ -13,51 +12,26 @@ const CartStackNavigator = () => {
   const navigation = useNavigation();
   const darkMode = useTheme();
 
-  const headerStyles = {
-    headerTintColor: darkMode ? 'white' : '#eda6c2',
-    headerStyle: {
-      backgroundColor: darkMode ? '#1B1A17' : '#9C0F48',
-    },
-    headerTitleStyle: {
-      fontWeight: '700',
-    },
-  };
-
   return (
     <Stack.Navigator initialRouteName="Cart">
       <Stack.Screen
         name="CartPage"
         component={Cart}
-        options={{
-          ...headerStyles,
-          headerLeft: props => (
-            <Header
-              iconName="cart"
-              size={30}
-              theme={darkMode ? 'white' : '#eda6c2'}
-              onPress={undefined}
-              name={'Cart'}
-              variant="Bold"
-            />
-          ),
-          title: '',
-        }}
+        options={() =>
+          headerReturn('cart', () => navigation.goBack(), 'Cart', darkMode)
+        }
       />
       <Stack.Screen
         name="Product Details"
         component={ProductDetails}
-        options={{
-          ...headerStyles,
-          headerLeft: props => (
-            <Ionicons
-              name="arrow-back-circle-outline"
-              size={30}
-              color={darkMode ? 'white' : '#eda6c2'}
-              style={{marginEnd: 10}}
-              onPress={() => navigation.navigate('CartPage')}
-            />
-          ),
-        }}
+        options={() =>
+          headerReturn(
+            'arrow-back-circle-outline',
+            () => navigation.navigate('CartPage'),
+            '',
+            darkMode,
+          )
+        }
       />
     </Stack.Navigator>
   );
