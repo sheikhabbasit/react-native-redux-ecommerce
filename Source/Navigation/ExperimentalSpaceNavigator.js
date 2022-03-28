a768c419e51dd0597202fe0f1626e18e1d8e6784
sheikhabbasit <sheikh.abdul.basit95@gmail.com>
2022-03-28 12:15:35 +0530

    refactor(ExperimentalStackNavigator): remove headerstyles object, minimised props for header

--- Source/Navigation/ExperimentalSpaceNavigator.js
+++ Source/Navigation/ExperimentalSpaceNavigator.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import Ionicons from 'react-native-vector-icons/Ionicons';
 import {useNavigation} from '@react-navigation/native';
 import ImageHome from '../Screens/ImageHome';
 import ImageGenre from '../Screens/ImageGenre';
@@ -10,144 +9,100 @@ import Pagination from '../Screens/Pagination';
 import {useTheme} from '../Hooks/useTheme';
 import Map from '../Screens/Map';
 import LocationSelect from '../Screens/LocationSelect';
-import Header from '../Components/Typography/Header';
+import {headerReturn} from '../HelperFunctions/HeaderReturn';
 
 const Stack = createNativeStackNavigator();
 
 const ExperimentalStackNavigator = () => {
   const navigation = useNavigation();
   const darkMode = useTheme();
-  const headerStyles = {
-    headerTintColor: darkMode ? 'white' : '#eda6c2',
-    headerStyle: {
-      backgroundColor: darkMode ? '#1B1A17' : '#9C0F48',
-    },
-    headerTitleStyle: {
-      fontWeight: '700',
-    },
-    title: '',
-  };
 
   return (
     <Stack.Navigator initialRouteName="Image Home">
       <Stack.Screen
         name="Image Home"
         component={ImageHome}
-        options={{
-          ...headerStyles,
-          headerLeft: props => (
-            <Header
-              iconName="md-images"
-              size={30}
-              theme={darkMode ? 'white' : '#eda6c2'}
-              onPress={() => navigation.navigate('Image Genre')}
-              name={'Images'}
-              variant="Bold"
-            />
-          ),
-        }}
+        options={() =>
+          headerReturn(
+            'md-images',
+            () => navigation.navigate('Image Genre'),
+            'Images',
+            darkMode,
+          )
+        }
       />
       <Stack.Screen
         name="Image Genre"
         component={ImageGenre}
-        options={{
-          ...headerStyles,
-          headerLeft: props => (
-            <Header
-              iconName="arrow-back-circle-outline"
-              size={30}
-              theme={darkMode}
-              onPress={() => navigation.navigate('Image Home')}
-              name={'Image Genre'}
-              variant="Bold"
-            />
-          ),
-        }}
+        options={() =>
+          headerReturn(
+            'arrow-back-circle-outline',
+            () => navigation.navigate('Image Home'),
+            'Images by Genre',
+            darkMode,
+          )
+        }
       />
       <Stack.Screen
         name="Image With Breed"
         component={ImageWithBreed}
-        options={{
-          ...headerStyles,
-          headerLeft: props => (
-            <Header
-              iconName="arrow-back-circle-outline"
-              size={30}
-              theme={darkMode}
-              onPress={() => navigation.navigate('Image Home')}
-              name={'Images of Dog Breed'}
-              variant="Bold"
-            />
-          ),
-        }}
+        options={() =>
+          headerReturn(
+            'arrow-back-circle-outline',
+            () => navigation.navigate('Image Home'),
+            'Images of Dog Breed',
+            darkMode,
+          )
+        }
       />
       <Stack.Screen
         name="Countries"
         component={Countries}
-        options={{
-          ...headerStyles,
-          headerLeft: props => (
-            <Header
-              iconName="arrow-back-circle-outline"
-              size={30}
-              theme={darkMode}
-              onPress={() => navigation.navigate('Image Home')}
-              name={'Countries'}
-              variant="Bold"
-            />
-          ),
-        }}
+        options={() =>
+          headerReturn(
+            'arrow-back-circle-outline',
+            () => navigation.navigate('Image Home'),
+            'Countries',
+
+            darkMode,
+          )
+        }
       />
       <Stack.Screen
         name="Image With Pagination"
         component={Pagination}
-        options={{
-          ...headerStyles,
-          headerLeft: props => (
-            <Header
-              iconName="arrow-back-circle-outline"
-              size={30}
-              theme={darkMode}
-              onPress={() => navigation.navigate('Image Home')}
-              name={'Images With Pagination'}
-              variant="Bold"
-            />
-          ),
-        }}
+        options={() =>
+          headerReturn(
+            'arrow-back-circle-outline',
+            () => navigation.navigate('Image Home'),
+            'Images by Pagination',
+            darkMode,
+          )
+        }
       />
       <Stack.Screen
         name="Location Select"
         component={LocationSelect}
-        options={{
-          ...headerStyles,
-          headerLeft: props => (
-            <Header
-              iconName="arrow-back-circle-outline"
-              size={30}
-              theme={darkMode}
-              onPress={() => navigation.navigate('Image Home')}
-              name={'Countries'}
-              variant="Bold"
-            />
-          ),
-        }}
+        options={() =>
+          headerReturn(
+            'arrow-back-circle-outline',
+            () => navigation.navigate('Image Home'),
+            'Location Select',
+            darkMode,
+          )
+        }
       />
       <Stack.Screen
         name="Map"
         component={Map}
-        options={{
-          ...headerStyles,
-          headerLeft: props => (
-            <Header
-              iconName="arrow-back-circle-outline"
-              size={30}
-              theme={darkMode ? 'white' : '#eda6c2'}
-              onPress={() => navigation.navigate('Image Home')}
-              name={'Maps'}
-              variant="Bold"
-            />
-          ),
-        }}
+        options={() =>
+          headerReturn(
+            'arrow-back-circle-outline',
+            () => navigation.goBack(),
+            'Map',
+            darkMode,
+          )
+        }
       />
     </Stack.Navigator>
   );
