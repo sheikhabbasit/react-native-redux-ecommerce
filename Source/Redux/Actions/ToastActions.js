524469d35e90bdefe9c0d3597e85fae30f945f68
sheikhabbasit <sheikh.abdul.basit95@gmail.com>
2022-04-01 16:35:31 +0530

    feat(ToastActions): toasts for signup

--- Source/Redux/Actions/ToastActions.js
+++ Source/Redux/Actions/ToastActions.js
@@ -1,5 +1,7 @@
 export const ToastActions = {
   SET_LOGIN_ERROR: 'set_login_error',
   SET_LOGIN_SUCCESSFUL: 'set_login_successful',
+  SET_SIGNUP_SUCCESSFUL: 'set_signup_successful',
+  SET_SIGNUP_ERROR: 'set_signup_error',
   CLEAR: 'clear',
 };
