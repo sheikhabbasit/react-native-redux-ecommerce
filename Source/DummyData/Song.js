e13f32151d25f4649d75ab94528008cfddad5ca4
sheikhabbasit <sheikh.abdul.basit95@gmail.com>
2022-04-12 18:46:04 +0530

    feat(song.js): song file with timings and text

---
+++ Source/DummyData/Song.js
@@ -0,0 +1,38 @@
+export const song = [
+  {
+    text: '1. I am a text animator',
+    time: 1000,
+  },
+  {
+    text: '2. I am a disco dancer',
+    time: 2000,
+  },
+  {
+    text: '3. Zindagi mera gaana',
+    time: 3000,
+  },
+  {
+    text: '4. Mai kisi ka deewana',
+    time: 4000,
+  },
+  {
+    text: '5. To jhoomo, to naacho',
+    time: 5000,
+  },
+  {
+    text: '6. To mere saath naacho gaao',
+    time: 6000,
+  },
+  {
+    text: '7. To mere saath naacho gaao',
+    time: 7000,
+  },
+  {
+    text: '8. I am a disco dancer',
+    time: 8000,
+  },
+  {
+    text: '9. I am a disco dancer',
+    time: 9000,
+  },
+];
