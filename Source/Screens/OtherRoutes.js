ba21c1d4ea461477180584b66859d0a834f66bec
sheikhabbasit <sheikh.abdul.basit95@gmail.com>
2022-04-12 18:47:14 +0530

    style(OtherRoutes): add theme support

--- Source/Screens/OtherRoutes.js
+++ Source/Screens/OtherRoutes.js
@@ -1,51 +1,56 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 import Button from '../Components/HOC/Button';
+import Card from '../Components/HOC/Card';
 import FlexedScrollView from '../Components/HOC/FlexedScrollView';
+import {useTheme} from '../Hooks/useTheme';
 
 const OtherRoutes = props => {
+  const theme = useTheme();
   return (
-    <FlexedScrollView>
-      <Button
-        color="#062C30"
-        label="List of Dog Breeds"
-        onPress={() => props.navigation.navigate('Image Genre')}
-      />
-      <Button
-        color="#062C30"
-        label="List of Countries"
-        onPress={() => props.navigation.navigate('Countries')}
-      />
-      <Button
-        color="#062C30"
-        label="Pagination Flatlist"
-        onPress={() => props.navigation.navigate('Image With Pagination')}
-      />
-      <Button
-        color="#062C30"
-        label="Map"
-        onPress={() => props.navigation.navigate('Location Select')}
-      />
-      <Button
-        color="#062C30"
-        label="Direct to map"
-        onPress={() => props.navigation.navigate('Map')}
-      />
-      <Button
-        color="#062C30"
-        label="Animations"
-        onPress={() => props.navigation.navigate('Animations')}
-      />
-      <Button
-        color="#062C30"
-        label="Stepper"
-        onPress={() => props.navigation.navigate('Stepper')}
-      />
-      <Button
-        color="#062C30"
-        label="Text Animator"
-        onPress={() => props.navigation.navigate('Text Animator')}
-      />
+    <FlexedScrollView variant="#062C30">
+      <Card theme={theme}>
+        <Button
+          color="#062C30"
+          label="List of Dog Breeds"
+          onPress={() => props.navigation.navigate('Image Genre')}
+        />
+        <Button
+          color="#062C30"
+          label="List of Countries"
+          onPress={() => props.navigation.navigate('Countries')}
+        />
+        <Button
+          color="#062C30"
+          label="Pagination Flatlist"
+          onPress={() => props.navigation.navigate('Image With Pagination')}
+        />
+        <Button
+          color="#062C30"
+          label="Map"
+          onPress={() => props.navigation.navigate('Location Select')}
+        />
+        <Button
+          color="#062C30"
+          label="Direct to map"
+          onPress={() => props.navigation.navigate('Map')}
+        />
+        <Button
+          color="#062C30"
+          label="Animations"
+          onPress={() => props.navigation.navigate('Animations')}
+        />
+        <Button
+          color="#062C30"
+          label="Stepper"
+          onPress={() => props.navigation.navigate('Stepper')}
+        />
+        <Button
+          color="#062C30"
+          label="Text Animator"
+          onPress={() => props.navigation.navigate('Text Animator')}
+        />
+      </Card>
     </FlexedScrollView>
   );
 };
